0c0f7dc3ad57a4cb9b092606862aa4dbd7b6b172
Dynamicwebbuilders1 <99330032+Dynamicwebbuilders1@users.noreply.github.com>
2022-02-10 15:43:39 +0530

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,24 +1,16 @@
 <?php
-$name = $POST['name'];
-$visitor_email = $POST['email']
-$message = $POST['message']
-
-
-$email_from = 'praysnl1234xs@gmailc.com';
-
-$email_subject = "New Suspected Clients";
-
-$email_body = "User Name: $name.\n".
-                "User Email: $visitor_email.\n".
-                   "User Message: $message.\n;"
-
-        $to = "dynamicwebbuilders403@gmail.com";
-
-        $headers = "From: $email_from /r/n";
-        $headers .= "Reply-To: $visitor_email \r\n";
-
-        mail($to,$email_subject,$email_body,$headers);
-
-        header("Location: index.html");
-
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "dynamicwebbuilders403@gmail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:index.html");
 ?>
